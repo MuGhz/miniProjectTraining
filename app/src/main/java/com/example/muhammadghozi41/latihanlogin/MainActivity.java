9b167e568a4fb824b35f14e13b6073702f522742
Muhammad Ghozi <ghozi.mghozi@gmail.com>
2017-06-15 14:33:13 +0700

    check balance and top up

--- app/src/main/java/com/example/muhammadghozi41/latihanlogin/MainActivity.java
+++ app/src/main/java/com/example/muhammadghozi41/latihanlogin/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.muhammadghozi41.latihanlogin;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -13,16 +22,29 @@ import com.example.muhammadghozi41.latihanlogin.adapter.ListMenuAdapter;
 import com.example.muhammadghozi41.latihanlogin.model.ListMenuItem;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.example.muhammadghozi41.latihanlogin.R;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    final Context context = this;
+    private long balance;
+    public void initBalance(){
+        balance = 100000;
+    }
+    public void setBalance(long balance) {
+        this.balance = this.balance+balance;
+    }
+    public long getBalance() {
+        return balance;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initBalance();
         setTitle("Main Menu");
         String user = getIntent().getStringExtra("user");
         TextView txt = (TextView) findViewById(R.id.text);
@@ -38,10 +60,61 @@ public class MainActivity extends AppCompatActivity {
         listView.setAdapter(listMenuAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String label = listMenuAdapter.getItem(i).getLabel().toString();
                 Toast.makeText(MainActivity.this, label, Toast.LENGTH_SHORT).show();
+                
+                if(label.equals("Check Balance")){
+                    AlertDialog.Builder builder =  new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("Your Balance")
+                            .setMessage(getBalance()+"")
+                            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                }
+                            })
+                            .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.cancel();
+                                }
+                            })
+                            .create().show();
+                }
+
+                if(label.equals("Top Up")){
+                    LayoutInflater li = LayoutInflater.from(context);
+                    View topUpDialog = li.inflate(R.layout.dialog_top_up,null);
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setView(topUpDialog);
+                    builder.setTitle("Top Up your balance");
+                    final EditText userInput = (EditText) topUpDialog
+                            .findViewById(R.id.editTextDialogTopUp);
+                    builder.setPositiveButton("OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,int id) {
+                                    setBalance(Long.parseLong(userInput.getText().toString()));
+                                    Toast.makeText(context,"Top Up Success, your balance is "+getBalance(),Toast.LENGTH_LONG).show();
+                                }
+                            })
+                            .setNegativeButton("Cancel",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog,int id) {
+                                            dialog.cancel();
+                                        }
+                                    });
+                    AlertDialog topUpAlert = builder.create();
+                    topUpAlert.show();
+                }
+
+                if(label.equals("Log Out")){
+
+                }
+
                 }
         });
     }
